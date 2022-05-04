4c5dcba4a3f2a736f1ed9dc5428bc40a57c58520
bashthat <3249@holbertonschool.com>
2022-05-03 22:39:36 -0500

    spacing

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for x in range(1, 101):
         if x % 3 == 0 and x % 5 == 0:
-            print("{} ".format("FizzBuzz"), end=" ")
+            print("{}".format("FizzBuzz"), end=" ")
         elif x % 3 == 0:
-            print("{} ".format("Fizz"), end=" ")
+            print("{}".format("Fizz"), end=" ")
         elif x % 5 == 0:
-            print("{} ".format("Buzz"), end=" ")
+            print("{}".format("Buzz"), end=" ")
         else:
-            print("{} ".format(x), end="")
+            print("{}".format(x), end="")
