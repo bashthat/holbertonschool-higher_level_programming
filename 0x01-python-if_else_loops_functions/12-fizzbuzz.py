53631ad58bf7745b2e29d598fde69d0e2e2001c6
bashthat <3249@holbertonschool.com>
2022-05-03 22:08:29 -0500

    comments work

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for num in range(1, 101):
+        if num % 15 == 0:
+            print("FizzBuzz", end=" ")
         if num % 3 == 0:
             print("Fizz", end=" ")
         if num % 5 == 0:
             print("Buzz", end=" ")
-        if num % 15 == 0:
-            print("FizzBuzz", end=" ")
+       # if num % 15 == 0:
+        #    print("FizzBuzz", end=" ")
         else:
             print(num, end=" ")
