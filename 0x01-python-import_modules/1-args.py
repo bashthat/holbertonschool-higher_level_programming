82e181348efc29c9db482d8265f64845d0908639
bashthat <3249@holbertonschool.com>
2022-05-04 22:07:46 -0500

    edits

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-def num_of_args(*args):
-    return(len(args))
-print(num_of_args())
+if __name__ == "__main__":
+    from sys import argv
+    import sys
+    if len(sys.argv) == 1:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+    else:
+        if len(sys.argv) == 2:
+            print("{:d} argument:".format(len(sys.argv) - 1))
+        else:
+            print("{:d} arguments:".format(len(sys.argv) - 1))
+        for av in range(1, len(sys.argv)):
+            print("{:d}: {}".format(av, (sys.argv[av])))
