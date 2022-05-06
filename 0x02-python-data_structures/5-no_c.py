05b33b2eb0e5d38e0d1b8c3dd391a25951ec1d00
bashthat <3249@holbertonschool.com>
2022-05-05 21:46:44 -0500

    simplifysyntax

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     string_xyz = ""
-    for r in range(len(my_string)):
-        if my_string[r] != "C" and my_string[r] != "c":
-            string_xyz += my_string[r]
+    for r in my_string:
+        if r != "C" and r != "c":
+            string_xyz += r
             return string_xyz
