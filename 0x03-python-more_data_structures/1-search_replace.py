aebbe0c723bb0cc0c11867f74843dcbf10e9a434
bashthat <3249@holbertonschool.com>
2022-05-09 16:19:45 -0500

    edits

---
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for xyz in range(len(new_list)):
+        if new_list[xyz] == search:
+            new_list[xyz] = replace
+            return new_list
---
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    for xyz in set(my_list):
+        unique += xyz
+        return unique
