5b0c8504934dd7c8a5b8eb1e12f2130d695d6848
bashthat <3249@holbertonschool.com>
2022-05-09 16:26:59 -0500

    quickfix

--- 0x03-python-more_data_structures/2-uniq_add.py
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique = 0:
-    xyz = set(my_list):
-        for add in xyz:
+    unique = 0
+    xyz = set(my_list)
+    for add in xyz:
         unique += add
         return unique
