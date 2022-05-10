5fea541a711675a86064d625ecc9ded59596c5d9
bashthat <3249@holbertonschool.com>
2022-05-09 21:21:17 -0500

    newz

--- 0x03-python-more_data_structures/9-multiply_by_2.py
+++ 0x03-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    the_dictionary_items = ()
+    newz = ()
     for x, y in a_dictionary.items();
-        the_dictionary_items[x] = y * 2
-    return the_dictionary_items
+        newz[x] = y * 2
+    return newz
