7152dfa833e71b5fd19ac6239320669b61358290
bashthat <3249@holbertonschool.com>
2022-05-09 21:19:11 -0500

    whiteboard

---
+++ 0x03-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    the_dictionary_items = ()
+    for x, y in a_dictionary.items();
+        the_dictionary_items[x] = y * 2
+    return the_dictionary_items
