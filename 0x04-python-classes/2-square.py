8c7a3f554ed14f9e3185175d8f3506f7e8bb016f
bashthat <3249@holbertonschool.com>
2022-05-10 21:47:56 -0500

    spacinggit add 2-square.py

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-"""the size of a square is crucial to the python in the box!"""
-
 
 class Square:
-    """a simple Square that defines the size
-    """
+    
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
-    def area(self):
-        """ returns the area """
-        return (self.__size ** 2)
 
-    @size.setter
+        def area(self):
+            
+
+            return self.__size ** 2
+
+        @property
+        def size(self):
+
+
+            return self.__size
+
+
+        @size.setter
+
         def size(self, value):
-        """setter for the size of the square input"""
-
-            if type(size) != int:
-       	        raise TypeError("size must be an integer")
-            if size < 0:
+            if type(value) != int:
+                raise TypeError("size must be an integer")
+            if value < 0:
                 raise ValueError("size must be >= 0")
-            self.__size = size
-
-
-    @property
-    def size(self):
-
-
-        return self.__size
+            self.__size = value
