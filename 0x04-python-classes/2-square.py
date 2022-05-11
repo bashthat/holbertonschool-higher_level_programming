f39f3202d407a4aff2709454eef148dd3c382fae
bashthat <3249@holbertonschool.com>
2022-05-10 23:12:18 -0500

    spacing

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -18,12 +18,12 @@ class Square:
 
     def __init__(self, size=0):
         if type(size) != int:
-            raise TypeError( "size must be an integer" )
+            raise TypeError("size must be an integer")
         """
         tips given on proper syntax
         """
         if size < 0:
-            raise ValueError( "size must be >= 0" )
+            raise ValueError("size must be >= 0")
         self.__size = size
 
         """
@@ -34,4 +34,4 @@ class Square:
                 Returns:
                     int : The area of the square.
                 """
-            return(self.__size**2)
+            return(self.__size * self.__size)
