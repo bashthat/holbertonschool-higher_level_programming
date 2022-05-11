cfb0e93b2a957c72114ec727d75194a4a17b7e07
bashthat <3249@holbertonschool.com>
2022-05-10 20:15:26 -0500

    PEPedits

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -10,6 +10,7 @@ def __init__(self, size=0):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
+    self.__size = size
+    
     def area(self):
-        return(self.__area = area)
+        return(self.__size * self.__size)
---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""the size of a square is crucial to the class"""
+
+
+
+class Square
+"""a simple squre that defines the private instance of size"""
+def __init__(self, size=0):
