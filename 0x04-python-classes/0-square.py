e28692ca4faa743f82fdd0578721943a2cee2974
bashthat <3249@holbertonschool.com>
2022-05-10 19:58:59 -0500

    self.__surf = surf

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""The size of a square is crucial for a square, many things depend of it (area computation, etc.), so you, as class builder, must control the type and value of this attribute. """
+"""Simple Square class defines a square and its size always"""
+
+
 class Square:
-    """ a simple Square that defines the private instance of size, and instantiation with size.
-    
-    """
-    def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+    """the size of the square is crucial in this respect."""
+    def __init__(self, size):
         self.__size = size
+    
