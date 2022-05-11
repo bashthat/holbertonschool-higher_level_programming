63a94643a1a3fd0656bbcd6aa37c642a7b1d87a2
bashthat <3249@holbertonschool.com>
2022-05-10 23:48:00 -0500

    definition

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -11,7 +11,7 @@ class Square:
 		if size < 0:
 			raise ValueError("size must be >= 0")
 		self.__size = size
-
+        """the area is defined!"""
 	def area(self):
 		"""Finds the area of a square
 		Returns:
