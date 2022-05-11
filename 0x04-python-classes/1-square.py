ce2e3b90d46c7b2702cb406c26f9b7dc212275df
bashthat <3249@holbertonschool.com>
2022-05-10 19:32:22 -0500

    developed1in0

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
-def __init__(self, size=0):
-class Size
-    """utilizing the self from basic example in ww3.ntu.edu.sg"""
+"""The size of a square is crucial for a square, many things depend of it (area computation, etc.), so you, as class builder, must control the type and value of this attribute. """
+class Square:
+    """ a simple Square that defines the private instance of size, and instantiation with size.
+    
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
