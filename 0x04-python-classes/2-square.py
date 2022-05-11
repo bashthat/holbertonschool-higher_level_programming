64d8e714d5a0eacfbcff177bc6cf5eb21a1ef802
bashthat <3249@holbertonschool.com>
2022-05-10 20:34:31 -0500

    PEP8

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -16,6 +16,18 @@ def __init__(self, size=0):
         """ returns the area """
         return (self.__size ** 2)
 
+    @size.setter
+    def size(self, value):
+        """setter for the size of the square input"""
+
+
+
+    if type(size) != int:
+       	raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
+
 
     @property
     def size(self):
