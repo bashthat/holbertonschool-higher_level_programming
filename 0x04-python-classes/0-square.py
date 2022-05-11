49da894173c1455f3ceae93985fefde170d2eda7
bashthat <3249@holbertonschool.com>
2022-05-10 19:28:37 -0500

    typeerrors

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -3,7 +3,12 @@
 
 class Square:
     """ a simple Square that defines the private instance of size, and instantiation with size.
-
-    
     """
-    pass
+    def __init__(self, size=0):
+        """ utilizing type errors """
+        
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
