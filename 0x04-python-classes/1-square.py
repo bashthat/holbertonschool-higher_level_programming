e15fc327831351c8a4b6489c771e14bb5abaab67
bashthat <3249@holbertonschool.com>
2022-05-10 19:34:07 -0500

    edits

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""The size of a square is crucial for a square, many things depend of it (area computation, etc.), so you, as class builder, must control the type and value of this attribute. """
+"""The size of a square is crucial for a square, control the type and value of this attribute. """
 class Square:
-    """ a simple Square that defines the private instance of size, and instantiation with size.
+    """ a simple Square that defines the private instance of size.
     
     """
     def __init__(self, size=0):
