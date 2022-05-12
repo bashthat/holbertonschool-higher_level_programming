67bf4e33471c491f3877fb30922f11c9879912fd
bashthat <3249@holbertonschool.com>
2022-05-11 20:21:44 -0700

    edits

--- 0x02-python-data_structures/11-delete_at.py
+++ 0x02-python-data_structures/11-delete_at.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    '''coding with py is fun'''
+    if idx < 0:
+        return (my_list)
+    '''if a list is found delete it!'''
+    if idx < len(my_list);
+        del(my_list[idx])
+    return (my_list)
