414500320386f97790d9d8e902f68bf32cb9fdd2
Sam <earthstar412@gmail.com>
2022-05-12 02:05:54 +0000

    edits

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -3,45 +3,35 @@
 
 
 class Square:
-	""" a simple Square that defines the private instance of size.
-            the size and area are being swapped hence the statement
-	"""
-	def __init__(self, size=0):
-            if type(size) != int:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+	""" a simple Square that defines the private instance of size."""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
         
 	 """print the actual square"""
         def my_print(self):
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
+            if self.size > 0:
+                for i in range(self.__size):
+                    for j in range(self.__size):
+                        print("#", end="")
                     print()
-                else
-                    print()
+            else:
+                print()
 
-        """the area is defined!"""
-	
         def area(self):
-		"""Finds the area of a square
-		Returns:
-			int: the squares area
-		"""
-		return self.__size ** 2
+            return(self.__size ** 2)
 
-            """need to return the size, no over-achieving!"""
-            @property
-            """returns the size of the square"""
-            def size(self):
-                return self.__size
+        @property
+        def size(self):
+            return(self.__size)
 
-            """set the size of the square with the call of size setter"""
-            @size.setter
-            def size(self, value)
-                if type(value) != int:
-                    raise TypeError("size must be an integer")
-                if value < 0:
-                    raise ValueError("size must be >= 0")
-                self.__size = value
+        @size.setter
+        def size(self, value)
+            if type(value) != int:
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
