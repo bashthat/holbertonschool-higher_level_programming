eebd726af312086993a25c04fdeee6115bf74cf8
bashthat <3249@holbertonschool.com>
2022-05-11 20:22:49 -0700

    colan!

--- 0x02-python-data_structures/11-delete_at.py
+++ 0x02-python-data_structures/11-delete_at.py
@@ -4,6 +4,6 @@ def delete_at(my_list=[], idx=0):
     if idx < 0:
         return (my_list)
     '''if a list is found delete it!'''
-    if idx < len(my_list);
+    if idx < len(my_list):
         del(my_list[idx])
     return (my_list)
