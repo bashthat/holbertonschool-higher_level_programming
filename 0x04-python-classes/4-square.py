ec3b73f2db4a78905ad87eb90092f5d032679464
bashthat <3249@holbertonschool.com>
2022-05-11 19:34:39 -0700

    edits!

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -1,18 +1,19 @@
-#!/usr/bin/python3
-'''the size of a square is crucial to the answer of the project'''
+ize of a square is crucial'''
 
 
 class Square:
-	''' a simple Square that defines the private instance of size.'''
+    ''' a simple Square that defines the private instance of size.'''
     def __init__(self, size=0):
+        '''initializes the size of the square.'''
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
         
-	''' this function prints the actual square '''
+	
         def my_print(self):
+            '''prints a square with the character #.'''
             if self.size > 0:
                 for i in range(self.__size):
                     for j in range(self.__size):
@@ -21,16 +22,17 @@ class Square:
             else:
                 print()
 
-    '''the actual area as input defined'''
         def area(self):
+            '''returns the area of the square.'''
             return(self.__size ** 2)
 
         @property
+            '''returns the size of the square.'''
         def size(self):
             return(self.__size)
 
         @size.setter
-        '''sets the size based on the input as a function to the square print'''
+            '''sets the size of the square.'''
         def size(self, value)
             if type(value) != int:
                 raise TypeError("size must be an integer")
