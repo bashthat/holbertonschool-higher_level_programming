e81b9d1058138ca066cb3886070ba4269323da63
bashthat <3249@holbertonschool.com>
2022-05-13 14:02:27 -0700

    newofolder

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''this is a prototype that returns an integer or float as part of a function '''
+
+
+def add_integer(a, b=98):
+    if type(a) == float:
+        a = int
+    elif type(a) != int:
+        raise TypeError ("a must be an integer")
+
+    if type(b) == float:
+        b = int
+    elif type(a) != int:
+        raise TypeError("b must be an integer")
+    '''adding both integers as a return'''
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,2 @@
+Python Test Driven Development.
+Working to test code with the doctest (problems 1-4) and unittest (problems 5+) function(S) to test the code in the learning process of this project/module.
