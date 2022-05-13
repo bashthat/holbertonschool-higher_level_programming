00ddee425c021904033882a36f0072a0171a9bb8
bashthat <3249@holbertonschool.com>
2022-05-13 14:55:32 -0700

    basics, and break

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 '''This is the matrix devided.. welcome to the new dimensions of Py! -- but this is the function of'''
 
+
+def matrix_divided(matrix, div):
+
+    TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+
+    TypeError("Each row of the matrix must have the same size")
+
+    TypeError("div must be a number")
+
+
+    ZeroDivisionError("division by zero")
