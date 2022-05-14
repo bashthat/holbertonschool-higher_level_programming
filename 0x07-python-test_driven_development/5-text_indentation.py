e0536c95c9302a43df2778d5fc6475a334ceedf9
bashthat <3249@holbertonschool.com>
2022-05-13 20:09:54 -0700

    edits

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''this is a test
+    Write a function that prints a text
+    with 2 new lines after each of these
+    characters: ., ? and :
+'''
+
+
+from operator import truediv
+
+
+def text_indentation(text):
+    if type(text) is not str or text =="":
+        raise TypeError("text must be a string")
+    spec_char = [".", "?", ":"]
+    while true:
+        for xlt in text:
+            if xlt in spec_char:
+                print(xlt)
+                print()
+                print('', end='', flush=True)
+
+            else:
+                print(xlt, end="", flush=True)
+        break
