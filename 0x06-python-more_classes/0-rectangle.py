425f16e01354be2ffb2375478e4bea2863ca9c6c
bashthat <3249@holbertonschool.com>
2022-05-16 15:17:36 -0700

    almost finished 100%

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -5,22 +5,25 @@
 class Rectangle:
     '''the dimensions of the triangle being described in a function'''
     def __init__(self, width=0, height=0):
-    
-        @property
+        
         def width(self):
             self.width = width
-    
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be >= 0")
-            self.width = width
-        @property
-        def height(self):
-            self.height = height    
+            self.height = height
         
-        if type(height) != int:
-            raise TypeError("width must be an integer")
-        if height <= 0:
-            raise ValueError("width must be >= 0")
+        @property
+        def area(self):
+            return self.width * self.height 
+        
+        @property
+        def width(self): 
+        
+            if type(width) != int:
+                raise TypeError("width must be an integer")
+            if width <= 0:
+                raise ValueError("width must be >= 0")
+        def height(self):
+            if type(height) != int:
+                raise TypeError("width must be an integer")
+            if height <= 0:
+                raise ValueError("width must be >= 0")
             self.height = height
