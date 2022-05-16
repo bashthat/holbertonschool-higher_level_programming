7bee193e0640d1ae309a6ea224186b27eaae9413
bashthat <3249@holbertonschool.com>
2022-05-16 13:01:06 -0700

    rectangular

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
-class Rectangle
 ''' this is a class initializing a Rectangle'''
 
 
-
+class Rectangle:
+    '''the dimensions of the triangle being described in a function'''
+    def __init__(self, width=0, height=0):
+    
+    @property
+        def width(self):
+            self.width = width
+    
+    @width.setter
+        def width(self):    
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be >= 0")
+            self.width = width
+            
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be >= 0")
+            self.width = width
