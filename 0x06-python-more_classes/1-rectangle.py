aa7c214ee721ff40c9d90ec072ecb16215c95fbb
bashthat <3249@holbertonschool.com>
2022-05-17 13:18:12 -0700

    autopep

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -4,18 +4,19 @@
 
 class Rectangle:
     '''the dimensions of the triangle being defined through attributes'''
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
-            return (self.__width)
+        return (self.__width)
 
     @width.setter
     def width(self, value):
         '''setting the width of the rectangle'''
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -28,7 +29,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         '''setting the height of the equation'''
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
