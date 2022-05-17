aa7cc201754539c654c3dfba9b2ef6f0aa477054
bashthat <3249@holbertonschool.com>
2022-05-16 17:49:17 -0700

    organizing

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -11,19 +11,22 @@ class Rectangle:
             self.__height = height
         
         @property
-        def area(self):
-            return self.__width * self.__height 
+        def width(self):
+            return (self.__width)
         
-        @setter.width
-        
-        def width(self): 
-        
-            if type(width) != int:
+        @width.setter
+            def width(self, value)        
+            
+            if type(self) != int:
                 raise TypeError("width must be an integer")
             if width <= 0:
                 raise ValueError("width must be >= 0")
         
-        def height(self):
+        @property
+            def height(self):
+                return (self.__height)    
+        
+        def height(self, value):
             
             if type(height) != int:
                 raise TypeError("width must be an integer")
