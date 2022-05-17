0a48091b768015ba24e54f58cf9547d42ca7aad1
bashthat <3249@holbertonschool.com>
2022-05-16 18:14:34 -0700

    dimensions

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -5,7 +5,8 @@
 class Rectangle:
     '''the dimensions of the triangle being defined through defined attributes'''
     def __init__(self, width=0, height=0):
-        
+        self.__width = width
+        self.__height == height    
     
         @property
         def width(self):
