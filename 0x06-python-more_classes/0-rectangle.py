6a6e6e4dd2b3f6594f575a29b88b84a1152daf2f
bashthat <3249@holbertonschool.com>
2022-05-16 19:45:06 -0700

    indentation

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -7,25 +7,24 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        
+
     @property
     def width(self):
             return (self.__width)
 
     @width.setter
     def width(self, value):
-        '''setting the width of the rectangle'''        
+        '''setting the width of the rectangle'''       
         if type(value) != int:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
-        
-        
+
     @property
     def height(self):
         return (self.__height)
-        
+    
     @height.setter
     def height(self, value):
         '''setting the height of the equation'''
