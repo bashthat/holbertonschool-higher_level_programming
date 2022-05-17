6eb449e5edf80fc0356a1801391fff08a5e78a5e
bashthat <3249@holbertonschool.com>
2022-05-16 20:41:30 -0700

    misstyped loops

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -40,4 +40,4 @@ class Rectangle:
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * self.__width + 2 * self.__height
+        return (self.__width * 2) + (self.__height * 2)
