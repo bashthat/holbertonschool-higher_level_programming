dcf8de307ff29ae6ad851018f314f3a5bfa470da
bashthat <3249@holbertonschool.com>
2022-05-16 18:50:52 -0700

    edits

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
         @width.setter
         def width(self, value):
             self.__width = value        
-            if type(value) is not int:
+            if type(height) is not int:
                 raise TypeError("width must be an integer")
             if width < 0:
                 raise ValueError("width must be >= 0")
@@ -32,7 +32,7 @@ class Rectangle:
         @height.setter
         def height(self, value):
             self.__height = value
-            if type(value) is not int:
+            if type(height) is not int:
                 raise TypeError("height must be an integer")
             if height < 0:
                 raise ValueError("height must be >= 0")
