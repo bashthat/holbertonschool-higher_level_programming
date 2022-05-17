3d07b0aa16c2941c9bf86f9d34bba1b841c87c02
bashthat <3249@holbertonschool.com>
2022-05-16 21:31:45 -0700

    __str__(self)

--- 0x06-python-more_classes/3-rectangle.py
+++ 0x06-python-more_classes/3-rectangle.py
@@ -1,2 +1,58 @@
 #!/usr/bin/python3
-'''this is a rectangle class project'''
+''' this is a class initializing a Rectangle'''
+
+
+class Rectangle:
+    '''the dimensions of the triangle being defined through attributes'''
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+            return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        '''setting the width of the rectangle'''
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        '''setting the height of the equation'''
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def __repr__(self):
+        z = "Rectangle(" + str(self.__width) + ", " + str(self.__height) + ")"
+        return z
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        rectangle = ''
+        for x in range(self.__height):
+            for y in range(self.__width):
+                rectangle = rectangle + '#'
+            if x != self.__height - 1:
+                rectangle = rectangle + '\n'
+        return rectangle
