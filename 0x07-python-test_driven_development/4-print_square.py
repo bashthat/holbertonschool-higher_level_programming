cad93e8ba1f8fcfebd800d6b835c8d86514a2752
bashthat <3249@holbertonschool.com>
2022-05-17 13:01:05 -0700

    pep8

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,16 +3,16 @@
 printinng a square as part of the function for the benefit of testing the code
 '''
 
+
 def print_square(size):
     if type(size) != int or type(size) is float and size < 0:
         raise TypeError("size must be an integer")
-    
+
     if size == 0:
         None
 
     if size < 0:
         raise ValueError("size must be >= 0")
-    
 
     for xyz in range(size):
         for lemon in range(size):
