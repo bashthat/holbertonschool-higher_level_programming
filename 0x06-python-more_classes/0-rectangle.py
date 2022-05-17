5ff525c1a74ab2e7591b9c0c2ccf2f38c9209a61
bashthat <3249@holbertonschool.com>
2022-05-16 18:30:32 -0700

    pep8

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -7,10 +7,11 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        
         @property
         def width(self):
             return (self.__width)
-        
+
         @property
         def height(self):        
             return(self.__height)
