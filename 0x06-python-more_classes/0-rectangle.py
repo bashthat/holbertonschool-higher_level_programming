9b4f4ac8dfa5ed368cc0c902bd869734c6a4547b
bashthat <3249@holbertonschool.com>
2022-05-16 18:03:08 -0700

    organization

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-    ''' this is a class initializing a Rectangle'''
+''' this is a class initializing a Rectangle'''
 
 
 class Rectangle:
     '''the dimensions of the triangle being defined in through attributes'''
     def __init__(self, width=0, height=0):
         
-        def width(self):
-            self.__width = width
-            self.__height = height
-    ''' the definition of the objects IO '''     
+    
         @property
         def width(self):
             return (self.__width)
         
         @width.setter
-            def width(self, value)        
-            
+        def width(self, value):        
+            self.__width = width
+                
             if type(self) != int:
                 raise TypeError("width must be an integer")
             if width <= 0:
                 raise ValueError("width must be >= 0")
         
         @property
-            def height(self):
-                return (self.__height)    
+        def height(self):
+            return (self.__height)    
         
+        @height.setter
         def height(self, value):
             
             if type(self) != int:
                 raise TypeError("height must be an integer")
             if height <= 0:
                 raise ValueError("height must be >= 0")
-            self.__height = height
