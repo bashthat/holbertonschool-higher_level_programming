a263c990af64489928b3eb8d2c06c8f8b1b8b2ae
bashthat <3249@holbertonschool.com>
2022-05-16 18:47:51 -0700

    instructions

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
             self.__width = value        
             if type(value) is not int:
                 raise TypeError("width must be an integer")
-            if width <= 0:
+            if width >= 0:
                 raise ValueError("width must be >= 0")
             return("\n")
         
@@ -34,6 +34,6 @@ class Rectangle:
             self.__height = value
             if type(value) is not int:
                 raise TypeError("height must be an integer")
-            if height <= 0:
+            if height >= 0:
                 raise ValueError("height must be >= 0")
             return("\n")
