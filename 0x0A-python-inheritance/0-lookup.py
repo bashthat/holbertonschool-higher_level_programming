6c30f8c687ebd0dc7a136e9e3662a4e2d5123cd3
bashthat <3249@holbertonschool.com>
2022-05-18 01:58:32 -0700

    pep8attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''this is a function returning the avail list'''
+
+
+class Bass
+'''parent class'''
+
+
+def lookup(opj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+this is a project is on the bass class and what other classes inherit from the given class and the attributes of the class
