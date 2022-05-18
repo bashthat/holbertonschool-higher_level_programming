efd44a555085776902f84b9844a9b75cb1e25326
bashthat <3249@holbertonschool.com>
2022-05-18 08:42:10 -0700

    syntax_dir()

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,8 +2,6 @@
 '''this is a function returning the avail list'''
 
 
-def __init__(self, list):
-'''not sure if I need an object instead of list?'''
-
 def lookup(obj):
-	self.__obj == obj
+    '''returning the list through the object'''
+    return dir(obj)
