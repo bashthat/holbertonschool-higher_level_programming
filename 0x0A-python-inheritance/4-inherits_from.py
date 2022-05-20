b817b002bc208220d47b45d9d68f122c439ad05d
Sam <97474996+bashthat@users.noreply.github.com>
2022-05-19 23:47:05 -0500

    typos

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,6 @@
 
 def inherits_from(obj, a_class):
     '''returns True if the object is an instance of a class'''
-    if type(obj) != a_class:
-        return issubclass(type(obj), a_class)
-    else:
-        return false
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
