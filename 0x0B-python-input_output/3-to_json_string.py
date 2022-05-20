f67ddc6f4378d98eaa5a8dbde0a91a45db2010c1
bashthat <3249@holbertonschool.com>
2022-05-20 14:06:11 -0700

    myBestThought

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import json
+'''
+json import
+    definition and function
+    and returning text with
+    the function to_jons_string
+'''
+def to_json_string(my_obj):
+    return json.dumps(my_obj)
