1a23ce032c11786df2c77e2b24e0d2f516121636
bashthat <3249@holbertonschool.com>
2022-05-20 18:39:33 -0700

    edits to utf-8

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     as part of the json.dump function in the
     returned value of the function
     '''
-    with open(filename, "w") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return json.dump(my_obj, f)
