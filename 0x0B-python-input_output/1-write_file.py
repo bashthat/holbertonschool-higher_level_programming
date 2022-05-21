0010eee0e96ba9797fe9d448112a0277daff9a56
bashthat <3249@holbertonschool.com>
2022-05-20 21:44:36 -0700

    description edits

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ def write_file(filename="", text=""):
     '''
     open then write to text in the return
     '''
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
