dbd0c3ab3ab4b8d3362136b381beb12784eb9eb1
bashthat <3249@holbertonschool.com>
2022-05-20 18:33:57 -0700

    less note taking

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,8 +5,6 @@ java-script object notation
 import json
     '''
         definition and function
-        and returning text with
-        the function to_jons_string
     '''
 def to_json_string(my_obj):
     return json.dumps(my_obj)
