437a963d5d3cb904fbb7ea8b9df0f03d43d3574f
bashthat <3249@holbertonschool.com>
2022-05-20 21:52:10 -0700

    editing

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,7 +9,7 @@ def save_to_json_file(my_obj, filename):
     '''
     using the open function to utilize write
     as part of the json.dump function in the
-    returned value of the function
+    returned value of the args in the function
     '''
-    with open(filename, "w", encoding="utf-8") as f:
-        return json.dump(my_obj, f)
+    with open(filename, "w", encoding="utf-8") as fig:
+        return json.dump(my_obj, fig)
