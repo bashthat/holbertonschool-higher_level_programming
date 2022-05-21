72a4ccccca13a12b7828d9dc9c266883d9650181
bashthat <3249@holbertonschool.com>
2022-05-20 19:01:56 -0700

    edits to import

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+'''importing the operation calls for importing args to json file '''
 import sys
-
+import os.path
+import json
+save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
 '''
 utilizing the sys for writing into a file
 '''
-sys.path.append("5-save_to_json_file.py")
-sys.path.append("6-load_from_json_file.py")
+file = "add_item.json"
+json_list = []
+if os.path.exists(file):
+    json_list = load_from_json_file(file)
 
-        '''
-        from file import function
-        '''
+for xyz in range(1, len(sys.argv)):
+    json_list.append(sys.argv[xyz])
+
+    save_to_json_file(json_list, file)
