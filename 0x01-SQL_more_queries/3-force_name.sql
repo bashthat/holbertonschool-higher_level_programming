2e781a0aa5462485ab385bf88e8b5872187b3ba5
bashthat <3249@holbertonschool.com>
2022-06-09 21:52:44 -0700

    edits

---
+++ 0x01-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- write a script tht creates the taable force_name on the mySQL
+CREATE TABLE IF NOT EXISTS force_name (
+  id INT
+  name VARCHAR(256) NOT NULL
+);
---
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- california love
+SELECT cities.id, cities.name, states.name
+FROM cities INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
