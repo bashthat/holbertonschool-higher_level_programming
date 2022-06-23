28459c6831d455168635453d99549f17ef24e965
bashthat <3249@holbertonschool.com>
2022-06-23 14:48:43 -0500

    typo

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -11,13 +11,20 @@ import sys
 
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-
+    
+    db = MySQLdb.connect(
+    host='localhost',
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3],
+    charset='utf8',
+    )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
 
-    rows = cursor.fetchall()
-    for x in rows:
+    states = cursor.fetchall()
+    for x in states:
         print(x)
 
     cursor.close()
