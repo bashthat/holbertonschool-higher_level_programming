f371b4e852c3c0a9fcf04ba7a03770a37bb0608f
bashthat <3249@holbertonschool.com>
2022-06-23 14:47:03 -0500

    edtiing

---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+""" Creating the SQL table for the list of states in hbtn_0e_0_usa """
+
+import MySQLdb
+from sys import argv
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+    host='localhost',
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3],
+    charset='utf8',
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(sys.argv[4]))
+
+    states = cursor.fetchall()
+
+    for x in states:
+        print(x)
+    cursor.close()
+    db.close()
