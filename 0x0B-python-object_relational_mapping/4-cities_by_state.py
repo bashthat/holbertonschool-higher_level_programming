4ef510477767a81e20792f9a13db33a916acd1be
bashthat <3249@holbertonschool.com>
2022-06-23 15:09:03 -0500

    typo

---
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+""" Creating a script that lists all cities from the database hbtn_0e_4_usa"""
+
+import MySQLdb
+from sys import argv
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+    host='localhost',
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3],
+    charset='utf8',
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON states_id = city.state.id ORDER BY cities.id ASC")
+
+    states = cursor.fetchall()
+
+    for x in states:
+        print(x)
+    cursor.close()
+    db.close()
