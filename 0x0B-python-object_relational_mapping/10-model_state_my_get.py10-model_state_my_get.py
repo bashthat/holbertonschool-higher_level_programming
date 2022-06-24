08cae8b3a20dc5a3fb838cedecb9b0dd88b4d78e
bashthat <3249@holbertonschool.com>
2022-06-23 22:22:26 -0500

    colan

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py10-model_state_my_get.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     from sys import argv
     from model_state import Base, State
     from sqlalchemy.orm import Session
-    from sqlalchemy import create_engine
+    from sqlalchemy import (create_engine)
 
     engine = \
         create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
