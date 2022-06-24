0bc68e6ee28493db807a53cc20bc4faeb4e1f83a
bashthat <3249@holbertonschool.com>
2022-06-23 23:05:33 -0500

    formatting

---
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""new track on model_city"""
+
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+from relationship_state import Base, State
+
+
+class City(Base):
+
+    """
+    City base class
+    """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
