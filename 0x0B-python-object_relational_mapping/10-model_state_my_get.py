5668918fda9e78051afa550998c28edd20db4d5b
bashthat <3249@holbertonschool.com>
2022-06-23 22:30:00 -0500

    typos

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,19 +12,18 @@ if __name__ == '__main__':
     from sys import argv
     from model_state import Base, State
     from sqlalchemy.orm import Session
-    from sqlalchemy import create_engine
-    
+    from sqlalchemy import (create_engine)
+
     engine = \
         create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
                       sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    """
-    engine defined above
-    """
     Base.metadata.create_all(engine)
-
+    
     session = Session(engine)
-    
-    for state in session.query(State).filter(State.name.like("%a%")).order_by(State.id):
-        print ("{}: {}").format(state.id, state.name)
-    
+
+    state = session.query(State).filter(State.name==sys.argv[4]).first()
+    if state:
+        print ("{}").format(state.id)
+    else:
+        print ("Not found")
     session.close()
