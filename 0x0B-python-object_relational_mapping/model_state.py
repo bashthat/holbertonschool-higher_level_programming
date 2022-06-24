f2809720bcbf729a6bf7c35a775a1944bc709bdd
bashthat <3249@holbertonschool.com>
2022-06-23 23:23:59 -0500

    modified

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
+# -*- coding: utf-8 -*-
 
 """new track on model_state"""
 
 from enum import unique
 import sys
+import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sys import argv
@@ -20,5 +21,5 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
