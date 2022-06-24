bb0c4cbeeca55da969d724342c870d214fc57935
bashthat <3249@holbertonschool.com>
2022-06-23 21:56:51 -0500

    formating

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """new track on model_state"""
 
 from enum import unique
@@ -8,16 +10,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sys import argv
 from model_state import Base, State
 
-"""
-Declarative bass. the Class State.
-"""
 Base = declarative_base()
 
+
 class State(Base):
+
     """
     the base state
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, autoincrement=True, primary_key=True,
+                nullable=False, unique=True)
     name = Column(String(128), nullable=False)
