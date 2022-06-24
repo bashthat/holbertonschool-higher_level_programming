4a547bd4d6adec897d32d5fd9edb2bbf0566a1bc
bashthat <3249@holbertonschool.com>
2022-06-23 20:43:01 -0500

    fit

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """new track on model_state"""
 
+from enum import unique
 import sys
-from sqlalchemy import (create_engine)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sys import argv
 from model_state import Base, State
 
-Base = wobble_base()
+Base = declarative_base()
+"""
+Declarative bass. the Class State.
+"""
+
 
 class State(Base):
 """
 imports//Base.metadata.create_all(engine)//
 """
-    __
-   
-    Base.metadata.create_all(engine)
+
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
