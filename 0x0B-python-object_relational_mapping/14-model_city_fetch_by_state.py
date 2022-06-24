672f04a63914460d30175234943f921a573d3edf
bashthat <3249@holbertonschool.com>
2022-06-23 23:13:42 -0500

    editing

---
+++ 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+"""
+
+if __name__ == '__main__':
+
+    import sys
+    import sqlalchemy
+    from sys import argv
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy import create_engine
+
+    vtech = \
+        create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                      sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    
+    
+    Base.metadata.create_all(vtech)
+    Session = sessionmaker(bind=vtech)
+    session = Session()
+    rows = session.query(City, State).filter_by(City.state_id == State.id).order_by(City_id).all()
+    for city, state in rows:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
