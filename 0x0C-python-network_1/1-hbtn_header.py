aa2bab184211caf1d2336b3ec90c8da2e82459ab
bashthat <3249@holbertonschool.com>
2022-07-06 17:52:03 -0500

    imports

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -3,13 +3,13 @@
 Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id
 """
 
-
+import urllib
 import urllib.request
 import sys
-import os
+
 
 #creating the process
-#
+#imporing only urllib.request and sys
 
 if __name__ == "__main__":
     with urllib.request.urlopen("{}".format(sys.argv[1])) as response:
