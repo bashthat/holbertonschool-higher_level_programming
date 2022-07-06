da0c78ff7baef2f1ef394da80bdf90e426509110
bashthat <3249@holbertonschool.com>
2022-07-06 18:29:12 -0500

    editing

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -2,14 +2,20 @@
 """ 
 a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter
 """
-import email
-from urllib import request, parse
+
+import urllib
+import urllib.request as request
+import urllib.parse as parse
 from sys import argv
 
 if __name__ == "__main__":
-    data = parse.urlencode({"email": argv[2]}).encode()
-    rpost = request.Request(argv[1], data)
+    """taking in url/email"""
+    e_mail = {"email": argv[2]}
+    rpost = parse.urlencode(e_mail).encode('utf-8')
+    req = request.Request(argv[1], rpost)
+    
     """script displays the body of the response (decoded in utf-8)"""
-    with request.urlopen(rpost) as response:
+    with request.urlopen(req) as response:
+        
         """script"""
         print(response.read().decode("utf8"))
