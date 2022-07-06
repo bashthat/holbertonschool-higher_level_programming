c5b558160761a59c0320b3e44256f8926e135e30
bashthat <3249@holbertonschool.com>
2022-07-06 16:17:47 -0500

    edinting

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -7,19 +7,12 @@ networking! fetching https://intranet.hbtn.io/status
 import urllib.request
 
 
-def fetcher():
-    
-    
-if __name__ == "__main__":    
-    """
-    fetching the request.
-    """
-    import urllib.request as xyz
-        request = xyz.request ("https://intranet.hbtn.io/status")
-        with xyz.urlopen(request) as response:
-        """
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as xyz:
+        content = xyz.read()
         reading to print the response
-        """
+        #retrieving the content
+        
         site = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
