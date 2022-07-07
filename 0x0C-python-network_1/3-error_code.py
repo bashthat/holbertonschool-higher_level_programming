aa0c9fd316ba31773da13196e8d0db2f5d89ae0f
bashthat <3249@holbertonschool.com>
2022-07-06 19:46:32 -0500

    editing

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -9,6 +9,7 @@ from sys import argv
 if __name__ == '__main__':
     req = request.Request(argv[1])
     try:
+        """always try"""
         with request.urlopen(req) as r:
             print(r.read().decode('utf-8'))
     except error.HTTPError as e:
