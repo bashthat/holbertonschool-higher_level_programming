a8ee97aacb9ae8199a2cd73eb1d84cf9b2a6a770
bashthat <3249@holbertonschool.com>
2022-07-06 19:58:50 -0500

    r.text

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     req = requests.get('https://intranet.hbtn.io/status')
     text = req.text
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
