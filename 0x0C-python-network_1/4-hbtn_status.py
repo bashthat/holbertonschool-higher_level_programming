608dff4ce7f34115933562f23aaa8e3dcfa1ffec
bashthat <3249@holbertonschool.com>
2022-07-06 19:54:43 -0500

    editing indentation

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-import os
-import sys
-from sys import argv
-import urllib.request as request
 import requests
+
 """
 Write a Python script that fetches https://intranet.hbtn.io/status
 """
+
 if __name__ == '__main__':
 # retrieving the request
+
     req = requests.get('https://intranet.hbtn.io/status')
     text = req.text
     print("Body response:")
