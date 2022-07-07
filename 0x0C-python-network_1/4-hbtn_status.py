fa1ccb691dff59a29cf784e16b86a2243518e9a9
bashthat <3249@holbertonschool.com>
2022-07-06 20:07:51 -0500

    pep8

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from urllib import read
 import requests
+
 """
 Write a Python script that fetches https://intranet.hbtn.io/status
 """
 
 if __name__ == '__main__':
-# retrieving the request
 
-    r = requests.get('https://intranet.hbtn.io/status')
-    read = r.text
+    # retrieving the request
+
+    rx = requests.get('https://intranet.hbtn.io/status')
+    read = rx.text
     print("Body response:")
     print("\t- type: {}".format(type(read)))
     print("\t- content: {}".format(read))
