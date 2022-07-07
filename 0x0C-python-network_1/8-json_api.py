b98d24220e3e808605f0f545cc638a15bae8f264
bashthat <3249@holbertonschool.com>
2022-07-06 21:38:56 -0500

    editing the edits!

---
+++ 0x0C-python-network_1/8-json_api.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import queue
+import requests
+import sys
+import os
+
+
+if __name__ == "__main__":
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    req = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+
+    try:
+        val = req.json()
+        if val == {}:
+            
+            """
+            printing no result
+            """
+            
+            print("No result")
+        else:
+            print("[{}] {}".format(val.get("id"), val.get("name")))
+            """
+            value error
+            """
+    except ValueError:
+        print("Not a valid JSON")
