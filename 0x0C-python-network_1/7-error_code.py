2e5169f37f43af7d6e9b361546c5bcf7c1670351
bashthat <3249@holbertonschool.com>
2022-07-11 17:28:57 -0500

    editing

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -10,7 +10,7 @@ import sys
 
 if __name__ == "__main__":
     req = requests.get(sys.argv[1])
-    
+
     if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
     else:
