3bb5187be33d4299baa32d0e1a32c127e37706dc
bashthat <3249@holbertonschool.com>
2022-07-11 17:16:35 -0500

    pep8

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id
+Python script that takes in a URL, sends a request.
 """
 
 import urllib
@@ -8,8 +8,8 @@ import urllib.request
 import sys
 
 
-#creating the process
-#imporing only urllib.request and sys
+# the process
+# imporing only urllib.request and sys
 
 if __name__ == "__main__":
     with urllib.request.urlopen("{}".format(sys.argv[1])) as response:
