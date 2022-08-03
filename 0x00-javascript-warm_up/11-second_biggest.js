ffaf321e653fb7cb2f12e4a12e31d6b1ca46aee9
bashthat <3249@holbertonschool.com>
2022-08-02 21:18:27 -0500

    editing

---
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// finding the second biggest number
+function secondBiggest(arr) {
+    let biggest = arr[0];
+    let secondBiggest = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > biggest) {
+            secondBiggest = biggest;
+            biggest = arr[i];
+        } else if (arr[i] > secondBiggest) {
+            secondBiggest = arr[i];
+        }
+    }
+    return secondBiggest;
+}
