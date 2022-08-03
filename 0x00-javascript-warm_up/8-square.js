41c8107c642ede7c4a94d20d9632f53b80098021
bashthat <3249@holbertonschool.com>
2022-08-02 21:04:55 -0500

    printingsquare

---
+++ 0x00-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// printing a square
+const square = process.argv[2];
+if (isNaN(square)) {
+    console.log('Missing size');
+} else {
+    for (let x = 0; x < square; x++) {
+        console.log('X'.repeat(square));
+    }}
