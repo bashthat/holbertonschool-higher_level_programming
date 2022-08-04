267b67f7f4a7fba6ed1f734c0429740436f2cbf6
bashthat <3249@holbertonschool.com>
2022-08-04 17:50:29 -0500

    needed constants!

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 // utilizing argv and nested loops.
-if (process.argv.length === 2) {
-  console.log('No arguemnt');
-} else if (process.argv.length === 3) {
+const char = process.argv;
+const argslen = char.length;
+// handling the cases of the arguments
+if (argslen === 2) {
+  console.log('No argument');
+} else if (argslen === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
