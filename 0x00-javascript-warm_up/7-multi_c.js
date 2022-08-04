4ec063279d8b9dda75aac0ea470ccee0297dc4d8
bashthat <3249@holbertonschool.com>
2022-08-04 14:44:40 -0500

    redundant

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 // printing C is fun
-const array = process.argv[2]; {
-  if (isNaN(array)) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let x = array; x > 0; x--) {
-      console.log('C is fun');
-    }
+const array = process.argv[2];
+if (isNaN(array)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let x = array; x > 0; x--) {
+    console.log('C is fun');
   }
 }
