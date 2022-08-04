1d591786b668bfd83be52b73770386af2258c7a6
bashthat <3249@holbertonschool.com>
2022-08-04 17:59:00 -0500

    typo

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 // printing by use of an array
 const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < array.length; i++) {
+let i;
+for (i = 0; i < 3; i++) {
   console.log(array[i]);
 }
