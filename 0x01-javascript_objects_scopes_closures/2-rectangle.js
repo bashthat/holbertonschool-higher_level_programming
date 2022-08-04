ed6e861ec8f07e7119d070a6244bf95f35f87916
bashthat <3249@holbertonschool.com>
2022-08-04 18:14:10 -0500

    semistandard

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,6 +2,14 @@
 /* working on the module.exports methods */
 module.exports = class Rectangle {
   constructor (w, h) {
+    // this.width = w;
+    // this.height = h;
+    if (!w || !h) {
+      return;
+    }
+    if (w <= 0 || h <= 0) {
+      return;
+    }
     this.width = w;
     this.height = h;
   }
