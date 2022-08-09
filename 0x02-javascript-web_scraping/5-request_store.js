dd64656fc69c217d3ee1907210dbcf6a4637899b
bashthat <3249@holbertonschool.com>
2022-08-09 15:26:00 -0500

    edits

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -2,17 +2,35 @@
 // borrowing patterns of thought from previous exercises
 // count requests
 // counst fs
-const request = require('request');
+
 const fs = require('fs');
+// const url = process.argv[2];
+// const count = process.argv[3];
+const request = require('request');
+//request.get('http://loripsum.net/api', function (error, response, body) {
+request.get(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  fs.writeFile(process.argv[3], body, (error) => {
+    if (error) {
+      console.log(error);
+ // Print the google web page.
+  }});
+});
 // write file
-request.get(process.argv[2], function (err, response, bod) {
-  if (err) {
-    console.log(err);
-  } // error handling
+//request(url, function (err, response, bod) {
+//  if (err) {
+//    console.log(err);
+//  } else {
+  // error handling
   // handling arguments
-  fs.write(process.argv[3], bod, 'utf8', function (err) {
-    if (err) {
-      console.log(err);
-    } // error handling
-  });
-}); // word
+
+//    fs.writeFile(count, bod, 'utf8', function (err) {
+//      if (err) {
+//        console.log(err);
+//      }
+//    });
+//  } // error handling
+//}
+//); 
